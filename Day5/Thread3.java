75f3428f7737a8aaf080f1e24e02b43fae1017b1
Bhargavi-PS <bhargavips14@gmail.com>
2025-08-25 16:40:07 +0530

    Add files via upload

---
+++ Day5/Thread3.java
@@ -0,0 +1,23 @@
+class sample extends Thread
+{
+    sample(){
+            this.start();
+    } 
+    public void run()
+    {
+        System.out.println("welcome to thread"+Thread.currentThread().getName());
+   
+    }
+}
+ 
+class Thread3
+ {
+    public static void main(String asd[])
+    {
+       sample obb=new sample();
+       Thread.currentThread().setName("Main");
+       System.out.println(Thread.currentThread().getName());
+       
+     
+    }
+ }
